1b8dc27884fcc0a0e3200dd585be18591af430bb
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2023-09-14 11:16:46 +0530

    sec-1 done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 let a = document.getElementById("nav")
-let flow = document.querySelector(".comp-img")
+let flow = document.querySelector(".flow")
 function responsive() {
     a.classList.toggle("fix-lft")
     flow.classList.toggle("flow-hidden")
