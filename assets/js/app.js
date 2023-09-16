b2ff1619b301156e9e3ed598866272cc7918a356
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2023-09-16 15:41:37 +0530

    complte-sections done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
 let a = document.getElementById("nav")
 let flow = document.querySelector(".flow")
+let cross4 = document.querySelector(".cross-4")
+let cross5 = document.querySelector(".cross-5")
+let cross6 = document.querySelector(".cross-6")
+
 function responsive() {
     a.classList.toggle("fix-lft")
     flow.classList.toggle("flow-hidden")
+    cross4.classList.toggle("cross-1")
+    cross5.classList.toggle("cross-2")
+    cross6.classList.toggle("cross-3")
 }
